eab590da2f415684edc9002abe0a46d322694406
Padraic O'Neill <pconeill@gmail.com>
2017-03-18 18:02:22 -0600

    credit check complete, with check for AMEX

---
+++ credit_check.rb
@@ -0,0 +1,44 @@
+require 'pry'
+
+card_num = "342801633855673"
+
+#determine index range
+num_length = card_num.length
+if num_length.even?
+  num_range_extract = (0..num_length - 1)
+  num_range_remain = (1..num_length)
+else
+  num_range_extract = (1..num_length - 1)
+  num_range_remain = (0..num_length)
+end
+
+num_set = []
+num_set_two_digit = []
+singles_set = []
+
+#extract numbers and multiply
+num_range_extract.step(2).each { |index| num_set.push(card_num[index].to_i * 2)  }
+
+#select double digit numbers
+num_set_two_digit = num_set.select { |x| x > 9 }
+
+#remove double digit numbers
+num_set = num_set - num_set_two_digit
+
+#break down double digit numbers, add their parts, and put them back with num_set
+num_set_two_digit.map! { |x| x.to_s }
+num_set_two_digit.map! { |x| x[0].to_i + x[1].to_i }
+doubles_set = num_set + num_set_two_digit
+
+#grab the numbers still remaining from the original card number
+num_range_remain.step(2).each { |index| singles_set.push(card_num[index].to_i)  }
+
+#merge and sum the sets
+all_digits = singles_set + doubles_set
+sum = all_digits.inject(0) { |sum, x| sum + x}
+
+if sum % 10 == 0
+  puts "The number is valid."
+else
+  puts "Invalid number."
+end
